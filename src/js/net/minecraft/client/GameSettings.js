ce2f5699ba56584d5d0e2fd97b11af547ec5fce8
LabyStudio <labystudio@gmail.com>
2022-02-20 16:24:32 +0100

    fix cookie and font renderer bugs

--- src/js/net/minecraft/client/GameSettings.js
+++ src/js/net/minecraft/client/GameSettings.js
@@ -21,7 +21,7 @@ window.GameSettings = class {
 
     save() {
         for (let prop in this) {
-            document.cookie = prop + "=" + (this[prop] || "") + "; path=/";
+            document.cookie = prop + "=" + (this[prop] || "") + "; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT";
         }
     }
 
