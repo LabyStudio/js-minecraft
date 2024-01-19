LabyStudio/js-minecraft

commit a59538dd6eb325b6f4bee52b2d43406f6df4197b

corrected setBlockAt to allow for future implementation of storing only differences

src/js/net/minecraft/client/Minecraft.js
@@ -472,7 +472,7 @@ export default class Minecraft {
                         this.particleRenderer.spawnBlockBreakParticle(this.world, hitResult.x, hitResult.y, hitResult.z);
 
                         // Destroy block
-                        this.world.setBlockAt(hitResult.x, hitResult.y, hitResult.z, 0,1+this.isSingleplayer()?0:2);
+                        this.world.setBlockAt(hitResult.x, hitResult.y, hitResult.z, 0,1+(this.isSingleplayer()?0:2));
                         let face;
                         if(hitResult.face.y<0) face=0;
                         else if(hitResult.face.y>0) face=1;
@@ -565,7 +565,7 @@ export default class Minecraft {
                                     globalEval(code);
                                 }
                             }
-                            if(!onHit) this.world.setBlockAt(x, y, z, typeId,1+this.isSingleplayer()?0:2);
+                            if(!onHit) this.world.setBlockAt(x, y, z, typeId,1+(this.isSingleplayer()?0:2));
                             let face;
                             if(hitResult.face.y<0) face=0;
                             else if(hitResult.face.y>0) face=1;
@@ -583,7 +583,7 @@ export default class Minecraft {
                             if(!onHit) {
                                 // Handle block abilities
                                 let block = Block.getById(typeId);
-                                block.onBlockPlaced(this.world, x, y, z, hitResult.face,1+this.isSingleplayer()?0:2);
+                                block.onBlockPlaced(this.world, x, y, z, hitResult.face,1+(this.isSingleplayer()?0:2));
 
                                 // Play sound
                                 let sound = block.getSound();

blockly/scripts/craft_blocks.js
@@ -549,7 +549,7 @@ window.Block=Block;
         }
         // Spawn particle
         window.app.particleRenderer.spawnBlockBreakParticle(window.app.world,_x,_y,_z);
-        window.app.world.setBlockAt(_x,_y,_z, 0,1+window.app.isSingleplayer()?0:2); 
+        window.app.world.setBlockAt(_x,_y,_z, 0,1+(window.app.isSingleplayer()?0:2)); 
         window.app.player.digging(0,_x,_y,_z, 0);
       }
     }catch(e){}
@@ -561,7 +561,7 @@ window.Block=Block;
     return `
       {let typeId = window.app.player.inventory.getItemInSlot(`+value+`);
       window.app.player.inventorySelectSlot(`+value+`);
-      window.app.world.setBlockAt(_x, _y, _z, typeId,1+window.app.isSingleplayer()?0:2); 
+      window.app.world.setBlockAt(_x, _y, _z, typeId,1+(window.app.isSingleplayer()?0:2)); 
       window.app.player.placeBlock(_x,_y,_z,0,typeId,0,0,0);
       }
     `
@@ -681,14 +681,14 @@ window.Block=Block;
       {
         let typeId = window.app.player.inventory.getItemInSlot(`+value+`);
         window.app.player.inventorySelectSlot(`+value+`);
-        window.app.world.setBlockAt(_x+`+x+`, _y+`+y+`, _z+`+z+`, typeId,1+window.app.isSingleplayer()?0:2); 
+        window.app.world.setBlockAt(_x+`+x+`, _y+`+y+`, _z+`+z+`, typeId,1+(window.app.isSingleplayer()?0:2)); 
         window.app.player.placeBlock(_x+`+x+`, _y+`+y+`, _z+`+z+`,0, typeId,0,0,0)
       }
     `; 
     else return `
       {let typeId = window.app.player.inventory.getItemInSlot(`+value+`);
       window.app.player.inventorySelectSlot(`+value+`);
-      window.app.world.setBlockAt(`+x+`,`+y+`,`+z+`, typeId,1+window.app.isSingleplayer()?0:2); 
+      window.app.world.setBlockAt(`+x+`,`+y+`,`+z+`, typeId,1+(window.app.isSingleplayer()?0:2)); 
       window.app.player.placeBlock(`+x+`,`+y+`,`+z+`,0, typeId,0,0,0)
       }
     `;
@@ -715,7 +715,7 @@ window.Block=Block;
           }
           // Spawn particle
           window.app.particleRenderer.spawnBlockBreakParticle(window.app.world, _x+`+x+`, _y+`+y+`, _z+`+z+`);
-          window.app.world.setBlockAt(_x+`+x+`, _y+`+y+`, _z+`+z+`, 0,1+window.app.isSingleplayer()?0:2); 
+          window.app.world.setBlockAt(_x+`+x+`, _y+`+y+`, _z+`+z+`, 0,1+(window.app.isSingleplayer()?0:2)); 
           window.app.player.digging(0,_x+`+x+`, _y+`+y+`, _z+`+z+`, 0);
         }
       }catch(e){}
@@ -736,7 +736,7 @@ window.Block=Block;
       }
       // Spawn particle
       window.app.particleRenderer.spawnBlockBreakParticle(window.app.world, `+x+`,`+y+`,`+z+`);
-      window.app.world.setBlockAt(`+x+`,`+y+`,`+z+`, 0,1+window.app.isSingleplayer()?0:2); 
+      window.app.world.setBlockAt(`+x+`,`+y+`,`+z+`, 0,1+(window.app.isSingleplayer()?0:2)); 
       window.app.player.digging(0,`+x+`,`+y+`,`+z+`, 0);
     }catch(e){console.log(e)}
     `

src/js/net/minecraft/client/world/World.js
@@ -287,7 +287,7 @@ export default class World {
 
     setBlockAt(x, y, z, type,mode=0) {
         let chunk = this.getChunkAt(x >> 4, z >> 4);
-        chunk.setBlockAt(x & 15, y, z & 15, type,mode);
+        chunk.setBlockAt(x & 15, y, z & 15, type,0,mode);
 
         // Rebuild chunk
         this.onBlockChanged(x, y, z);

commit 05ab7507f5860d9c0847bad655852b63a7e38b09

corrected torch handling. error introduced in git revision a59538dd6eb325b6f4bee52b2d43406f6df4197b

src/js/net/minecraft/client/Minecraft.js
@@ -532,7 +532,7 @@ export default class Minecraft {
                             this.player.swingArm();
                             // Handle block abilities
                             let block = Block.getById(typeId);
-                            block.onBlockPlaced(this.world, x, y, z, hitResult.face,1+(this.isSingleplayer()?0:2));
+                            block.onBlockPlaced(this.world, x, y, z, hitResult.face,false,1+(this.isSingleplayer()?0:2));
 
                             // Play sound
                             let sound = block.getSound();